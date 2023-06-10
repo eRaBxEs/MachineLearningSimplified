d5120fe22551162eb4eebb4841e712f36f7880cd
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-10 08:44:37 +0100

    observe scatter plot before determing poly-regress

---
+++ step8-polynomial_regression/poly-regression-a.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+# The x-axis represents the hours of the day and the y-axis represents the speed:
+x = [1,2,3,5,6,7,8,9,10,12,13,14,15,16,18,19,21,22]
+y = [100,90,80,60,60,55,60,65,70,70,75,76,78,79,90,99,99,100]
+
+# Before anything try and  observe the scatter plot
+plt.scatter(x, y)
+plt.show()
