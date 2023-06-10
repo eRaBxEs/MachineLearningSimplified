90c42610714da84a74c79b473ccc1b31180703dd
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-06-10 08:25:54 +0100

    intro to scatter plot

---
+++ step6-scatter_plot/scatter-plot-a.py
@@ -0,0 +1,12 @@
+# A scatter plot is a diagram where each value in the data set is represented by a dot.
+# The Matplotlib module has a method for drawing scatter plots, it needs two arrays of the same length, 
+# one for the values of the x-axis, and one for the values of the y-axis
+
+import matplotlib.pyplot as plt
+
+x = [5,7,8,7,2,17,2,9,4,11,12,9,6] # The x array represents the age of each car.
+y = [99,86,87,88,111,86,103,87,94,78,77,85,86] # The y array represents the speed of each car.
+
+plt.scatter(x, y)
+
+plt.show()
